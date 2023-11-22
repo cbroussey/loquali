07133f3bcd99d3391761b239d84ae5a7beb0507e
salabiven <salome.abiven@etudiant.univ-rennes1.fr>
2023-11-22 14:55:28 +0100

    commit de la mort qui tue

---
+++ siteLinks/asset/js/modifInfosCompte.js
@@ -0,0 +1,15 @@
+function modifierInfos(button, champId) {
+        var champInfos = document.getElementById(champId);
+
+        if (button.innerHTML === "Modifier") {
+            // Transformation en champ de texte
+            champInfos.innerHTML = '<input type="text" id="nouveauNom" value="' + champInfos.innerHTML + '">';
+            button.innerHTML = "Enregistrer";
+        } else {
+            // Enregistrement du nouveau nom
+            var nouveauNom = document.getElementById("nouveauNom").value;
+            // Ici, tu pourrais envoyer le nouveau nom côté serveur avec une requête AJAX en utilisant PHP, mais pour l'exemple, on va le changer directement en JavaScript
+            champInfos.innerHTML = nouveauNom;
+            button.innerHTML = "Modifier";
+        }
+}
