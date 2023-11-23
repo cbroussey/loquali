31d0f860c9993052ece110ca6929148aa0c03d04
Colin Broussey <colin.broussey@etudiant.univ-rennes1.fr>
2023-11-23 12:30:36 +0100

    mise à jour de paiement.php et inclusion de header et de footer pour simplification

---
+++ siteLinks/footer.php
@@ -0,0 +1,28 @@
+<footer>
+    <div>
+      <div id="footerCercleLogo">
+        <img src="asset/img/logo.png" alt="logo">
+      </div>
+      <div id="footerText">
+        <div>
+          <h4>Nous contacter</h4>
+          <address>1, Rue édouard Branly, 22300 Lannion</address><br>
+          <a href="tel:+33296469300">02 96 46 93 00</a><br>
+          <a href="mailto:iut-lannion.univ-rennes.fr">iut-lannion.univ-rennes.fr</a>
+        </div>
+        <div>
+          <h4>Informations légales</h4>
+          <a href="">Plan du site</a><br>
+          <a href="">Mention légales</a><br>
+          <a href="">Condition générales de ventes</a><br>
+          <a href="">Données personnelles</a><br>
+          <a href="">Gestion de cookies</a><br>
+        </div>
+      </div>
+    </div>
+    <div>
+      <p>texte random</p>
+      <p>Copyright @ 2023 LoQuali.com</p>
+      <p>Suivez-nous !</p>
+    </div>
+</footer>
