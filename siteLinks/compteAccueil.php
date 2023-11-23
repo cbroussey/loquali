1d5676656bba29104d36fb61b871e9122a4bc944
salabiven <salome.abiven@etudiant.univ-rennes1.fr>
2023-11-23 09:00:10 +0100

    oui

--- siteLinks/compteAccueil.php
+++ siteLinks/compteAccueil.php
@@ -55,7 +55,7 @@
         <div class="container">
             <img src="asset/img/profils/photoProfil.png" alt="" id="photoProfil">
             <div class="middle">
-                <img src="asset/icons/photo.svg" alt="">
+                <img src="asset/icons/blanc/photo.svg" alt="" id="">
             </div>
         </div>
         <p class="bienvenue">Bienvenue ! Accédez à votre <a href="" class="lienPagePerso">page personnel</a>.</p>
@@ -125,6 +125,49 @@
         <a href="" id="comptePro">Passer à un compte propriétaire</a>
 
     </div>
+
+    <footer>
+
+        <div id="infosFooter">
+            <div id="footerCercleLogo" class="portableDroite">
+                <img src="asset/img/logo.png" alt="logo">
+            </div>
+            <div id="textefooter">
+                <div class="gauche" class="portableGauche" id="infosLegal">
+                    <h2>Informations légales</h2>
+                    <a href="">Plan du site</a>
+                    <a href="">Mentions légales</a>
+                    <a href="">Conditions générales de ventes</a>
+                    <a href="">Données personnelles</a>
+                    <a href="">Gestions des cookies</a>
+                </div>
+                <div class="centrer" class="portableDroite" id="support">
+                    <h2>Support client</h2>
+                    <a href="">Contacter le support</a>
+                </div>
+                <div class="centrer" class="portableDroite" id="reseaux">
+                    <h2>Suivez nous</h2>
+                    <div id="logoReseaux">
+                        <a href=""><img src="asset/icons/blanc/facebook.svg" alt=""></a>
+                        <a href=""><img src="asset/icons/blanc/instagram.svg" alt=""></a>
+                        <a href=""><img src="asset/icons/blanc/steam.svg" alt=""></a>
+                    </div>
+                </div>
+                <div class="droite" class="portableGauche" id="contact">
+                    <h2>Nous contacter</h2>
+                    <p>Rue Édouard Branly, 22300 Lannion</p>
+                    <p>02 96 46 93 00</p>
+                    <p>iut-lannion.univ-rennes.fr</p>
+                </div>
+            </div>
+        </div>
+
+        <div>
+            <p>Copyright @ 2023 LoQuali.com</p>
+        </div>
+
+    </footer>
+
     <script src="asset/js/header.js"></script>
 </body>
 </html>
