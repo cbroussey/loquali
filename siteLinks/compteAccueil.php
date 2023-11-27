9d85c756c425b15907bc52ad1249e9874e538e3b
salabiven <salome.abiven@etudiant.univ-rennes1.fr>
2023-11-27 11:30:26 +0100

    quoicoubeh ou quoi

--- siteLinks/compteAccueil.php
+++ siteLinks/compteAccueil.php
@@ -150,13 +150,11 @@
 
         </div>
         
-        <div class="separateur"></div>
+        <div class="separateurCompte"></div>
 
         <a href="" id="comptePro">Passer à un compte propriétaire</a>
 
     </div>
-
-
 
     <form method="post" id="popUpDeco">
         <div class="popUpDecoChoix">
@@ -167,50 +165,53 @@
             </div>
         </div>
   </form>
+ 
+  <footer>
 
-    <footer>
-
-    <div id="infosFooter">
+  <div id="infosFooter">
     <div id="footerCercleLogo" class="portableDroite">
         <img src="asset/img/logo.png" alt="logo">
     </div>
     <div id="textefooter">
-      <div class="gauche" class="portableGauche" id="infosLegal">
-          <h2>Informations légales</h2>
-          <a href="">Plan du site</a>
-          <a href="">Mentions légales</a>
-          <a href="">Conditions générales de ventes</a>
-          <a href="">Données personnelles</a>
-          <a href="">Gestions des cookies</a>
-      </div>
-      <div class="centrer" class="portableDroite" id="support">
-          <h2>Support client</h2>
-          <a href="">Contacter le support</a>
-      </div>
-      <div class="centrer" class="portableDroite" id="reseaux">
-          <h2>Suivez nous</h2>
-          <div id="logoReseaux">
-              <a href=""><img src="asset/icons/blanc/facebook.svg" alt=""></a>
-              <a href=""><img src="asset/icons/blanc/instagram.svg" alt=""></a>
-              <a href=""><img src="asset/icons/blanc/steam.svg" alt=""></a>
-          </div>
-      </div>
-      <div class="droite" class="portableGauche" id="contact">
-          <h2>Nous contacter</h2>
-          <p>Rue Édouard Branly, 22300 Lannion</p>
-          <p>02 96 46 93 00</p>
-          <p>iut-lannion.univ-rennes.fr</p>
-      </div>
-  </div>
+        <div class="gauche" class="portableGauche" id="infosLegal">
+            <h2>Informations légales</h2>
+            <a href="">Plan du site</a>
+            <a href="">Mentions légales</a>
+            <a href="">Conditions générales de ventes</a>
+            <a href="">Données personnelles</a>
+            <a href="">Gestions des cookies</a>
+        </div>
+        <div class="centrer" class="portableDroite" id="support">
+            <h2>Support client</h2>
+            <a href="">Contacter le support</a>
+        </div>
+        <div class="centrer" class="portableDroite" id="reseaux">
+            <h2>Suivez nous</h2>
+            <div id="logoReseaux">
+                <a href=""><img src="asset/icons/blanc/facebook.svg" alt=""></a>
+                <a href=""><img src="asset/icons/blanc/instagram.svg" alt=""></a>
+                <a href=""><img src="asset/icons/blanc/steam.svg" alt=""></a>
+            </div>
+        </div>
+        <div class="droite" class="portableGauche" id="contact">
+            <h2>Nous contacter</h2>
+            <p>Rue Édouard Branly, 22300 Lannion</p>
+            <p>02 96 46 93 00</p>
+            <p>iut-lannion.univ-rennes.fr</p>
+        </div>
+    </div>
 </div>
 
 <div class="basFooter">
-  <p>Copyright @ 2023 LoQuali.com</p>
+    <p>Copyright @ 2023 LoQuali.com</p>
 </div>
 
 </footer>
 
+
+
     <script src="asset/js/header.js"></script>
     <script src="asset/js/accountAccueil.js"></script>
 </body>
+
 </html>
