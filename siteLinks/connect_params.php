64694ed7d9250ac800c04d23784db79cabc8644c
emclugery <emma.clugery@etudiant.univ-rennes1.fr>
2023-11-27 10:29:11 +0100

    changement modification

--- siteLinks/connect_params.php
+++ siteLinks/connect_params.php
@@ -5,5 +5,55 @@ $server = 'servbdd';
 $driver = 'pgsql';
 $dbname = 'pg_emclugery';
 $user   = 'emclugery';
-$pass	= '!';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$pass	= 'Chouchig2/';
 ?>
