45ff2110148ab0108fd8d3d55f08e2ea735cb8fe
salabiven <salome.abiven@etudiant.univ-rennes1.fr>
2023-11-30 10:57:36 +0100

    des petits changements encore genre

--- siteLinks/compte.php
+++ siteLinks/compte.php
@@ -388,7 +388,27 @@
     </div>
 
     <div id="compteLogements">
-      <!-- Logements -->
+      <div class="lignes">
+          <p>Mot de passe</p>
+          <button class="modifications" id="modifmaj">Mettre à jour</button>
+        </div>
+
+        <div class="separateurgenre"></div>
+
+        <div class="lignes">
+          <p>Historique de l’appareil</p>
+          <p class="displayInfos">Session en cours</p>
+          <button class="modifications">Se déconnecter</button>
+        </div>
+
+        <div class="separateurgenre"></div>
+
+        <div class="lignes">
+          <p>Compte</p>
+          <p class="displayInfos">Désativez votre compte</p>
+          <button class="modifications">Désactiver</button>
+        </div>
+      </div>
     </div>
 
     <div id="compteReservations">
