377a8ec718b7eac7b7ad094193315b6b3aebbadf
aloger <alexandre.oger@etudiant.univ-rennes1.fr>
2024-01-23 15:24:48 +0100

    modif

---
+++ siteLinks/asset/js/ligneHorsligne.js
@@ -0,0 +1,8 @@
+function ouvreModal() {
+    document.getElementById('myModal2').style.display = 'block';
+}
+
+function fermeModal() {
+    document.getElementById('myModal2').style.display = 'none';
+}
+
