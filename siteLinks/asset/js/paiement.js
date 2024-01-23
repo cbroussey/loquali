deb7803a61cd353f7f9f18fac805dd5151a7aa02
Colin Broussey <colin.broussey@etudiant.univ-rennes1.fr>
2024-01-23 18:35:42 +0100

    sélection de cb enregistrée quasiment finie

--- siteLinks/asset/js/paiement.js
+++ siteLinks/asset/js/paiement.js
@@ -1,14 +1,43 @@
-cbSaved = []
+let cbSaved = {}
+
+function testCBexists() {
+    cardnum = document.getElementById("cardNumber").value
+    if (
+        cardnum in cbSaved
+        && cbSaved[cardnum]["crypto"] == document.getElementById("crypto").value
+        && cbSaved[cardnum]["valid"] == document.getElementById("expiry").value
+        ){
+        //document.querySelector("#paymentSaved > input").value = document.getElementById("cardNumber").value
+        document.getElementById("paymentSaved").innerHTML = contextMenus["CM3"].elems[Object.keys(cbSaved).indexOf(cardnum)]
+    } else {
+        document.getElementById("paymentSaved").innerHTML = '<input name="paymentSaved" class="inputImg" onclick="toggleCM(\'CM3\', document.querySelector(\'#paymentSaved\'))" value="Nouveau mode de paiement" readonly=""><img class="cmHideElem" src="asset/img/arrow-down.svg" onclick="toggleCM(\'CM3\', document.querySelector(\'#paymentSaved\'))">'
+    }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll("#CM3 > table > tbody > tr > td > input").forEach((e) => {
-        cbSaved.push(e.value)
-    })
+    let cbs = document.querySelectorAll("#CM3 > table > tbody > tr > td > .inputImg")
+    let hiddendata = document.querySelectorAll('#CM3 > table > tbody > tr > td > input[type="hidden"]')
+    for (let i = 0; i < cbs.length; i++) {
+        cbSaved[cbs[i].value] = {}
+        cbSaved[cbs[i].value]["crypto"] = hiddendata[i*2].value
+        cbSaved[cbs[i].value]["valid"] = hiddendata[i*2+1].value.split("-").reverse().slice(1, 3)
+        cbSaved[cbs[i].value]["valid"][1] = cbSaved[cbs[i].value]["valid"][1].substr(2, 5)
+        cbSaved[cbs[i].value]["valid"] = cbSaved[cbs[i].value]["valid"].join("/")
+    }
     document.getElementById("cardNumber").addEventListener("input", () => {
-        if (cbSaved.includes(document.getElementById("cardNumber").value)) {
-            document.querySelector("#paymentSaved > input").value = document.getElementById("cardNumber").value
-        } else {
-            document.querySelector("#paymentSaved > input").value = "Nouveau type de paiement"
-        }
+        testCBexists()
     })
+    document.getElementById("expiry").addEventListener("input", () => {
+        testCBexists()
+    })
+    document.getElementById("crypto").addEventListener("input", () => {
+        testCBexists()
+    })
+    
+    document.querySelector("#paymentSaved > .inputImg").addEventListener("change", () => {
+        document.getElementById("cardNumber").value = document.querySelector("#paymentSaved > .inputImg").value
+        document.getElementById("expiry").value = cbSaved[document.querySelectorAll("#paymentSaved > .inputImg").value]["valid"]
+        document.getElementById("crypto").value = cbSaved[document.querySelectorAll("#paymentSaved > .inputImg").value]["crypto"]
+    })
+    
 })
