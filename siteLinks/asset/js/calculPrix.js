214edf9394ba0294fd0d43605373e1ac806920bd
moukhouyaali <moukhouyaali@etudiant.univ-rennes1.fr>
2024-01-24 15:18:36 +0000

    momo commit

---
+++ siteLinks/asset/js/calculPrix.js
@@ -0,0 +1,28 @@
+async function recupDate(idlog, datedeb, datefin){
+    let data = await fetch(`./calculPrix.php?id_logement=${idlog}&date_debut=${datedeb}&date_fin=${datefin}`)
+    data = await data.json()
+    return data
+}
+
+function setPrix() {
+    recupDate(document.getElementById("idlog").innerHTML, document.getElementById("start").value, document.getElementById("end").value).then((v) => {
+        document.querySelectorAll(".info_prix > .row > .value")[0].innerHTML = v["prix_ht"] + " €"
+        document.querySelectorAll(".info_prix > .row > .label")[0].innerHTML = v["nbjours"] + " nuits"
+        document.querySelectorAll(".info_prix > .row > .value")[2].innerHTML = v["prix_ht"] *0.10 + ".00 €";    
+        var prixHT = v["prix_ht"];
+    var montantTotal = prixHT * 1.10 + 29.96;
+
+document.querySelectorAll(".info_prix > .row > .value")[3].value = montantTotal.toFixed(2) + "€";
+
+    })
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("start").addEventListener("change", () => {
+        setPrix()
+    })
+    document.getElementById("end").addEventListener("change", () => {
+        setPrix()
+    })
+})
---
+++ siteLinks/calculPrix.php
@@ -0,0 +1,29 @@
+
+<?php 
+ include('connect_params.php');
+ $id = $_GET["id_logement"];
+ $date_debut = $_GET["date_debut"];
+ $date_fin = $_GET["date_fin"];
+ try {
+     $dbh = new PDO("$driver:host=$server;dbname=$dbname", $user, $pass);
+
+     $query1 = "SELECT * FROM test.getPlageData(:id_logement, :date_debut, :date_fin);";
+     $stmt = $dbh->prepare($query1);
+     $stmt->bindParam(':id_logement', $id, PDO::PARAM_INT);
+     $stmt->bindParam(':date_debut', $date_debut, PDO::PARAM_STR); 
+     $stmt->bindParam(':date_fin', $date_fin, PDO::PARAM_STR); 
+     $stmt->execute();
+     $info = $stmt->fetch(PDO::FETCH_ASSOC);
+     $info['date_debut'] = $date_debut;
+     $info['date_fin'] = $date_fin;
+ } catch (PDOException $e) {
+    print "Erreur !: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+echo json_encode($info);
+
+
+
+
+?>  
