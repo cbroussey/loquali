8b30c5da11639bf0fa2d6b45ef5d183ef2da898d
emclugery <emma.clugery@etudiant.univ-rennes1.fr>
2024-02-07 09:29:31 +0100

    logement avis pop up

--- siteLinks/asset/js/redirectionConnect.js
+++ siteLinks/asset/js/redirectionConnect.js
@@ -6,3 +6,11 @@ function refusRedirect() {
     document.getElementById('myModal4').style.display = 'none';
 }
 
+function modalRedirect2() {
+    document.getElementById('myModal5').style.display = 'block';
+}
+
+function refusRedirect2() {
+    document.getElementById('myModal5').style.display = 'none';
+}
+
